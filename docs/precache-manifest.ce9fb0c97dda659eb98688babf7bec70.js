393975109cba2566b12143cb6b7e653e974517e1
Memo <guillermosandoval_86@hotmail.com>
2020-09-14 10:16:31 -0500

    first commit

---
+++ docs/precache-manifest.ce9fb0c97dda659eb98688babf7bec70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0b93384053914acec2ba9dae1d9c47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9ba9d0f640427c6f130",
+    "url": "/static/css/main.989233ea.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ba9d0f640427c6f130",
+    "url": "/static/js/main.e15ce906.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
