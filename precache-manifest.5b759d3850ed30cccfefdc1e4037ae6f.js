279f4adaeb528d1d3ea4b6133394876ace6d4666
Memo <guillermosandoval_86@hotmail.com>
2020-12-18 10:48:27 -0600

    Updates

---
+++ precache-manifest.5b759d3850ed30cccfefdc1e4037ae6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f768899450f6032c0b903f09e75efc0",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "2e85109aadaf10360b2a",
+    "url": "/repository-name/static/css/main.989233ea.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/repository-name/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repository-name/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e85109aadaf10360b2a",
+    "url": "/repository-name/static/js/main.4d2952c0.chunk.js"
+  },
+  {
+    "revision": "e0d4b9b82dc59fe2e9a7",
+    "url": "/repository-name/static/js/runtime-main.aad7083c.js"
+  }
+]);
